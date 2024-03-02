1994240f2083c205167f5cb22b9436fc5e5e5f62
Nazrul Islam <nazrulislambhat@gmail.com>
2024-03-02 21:11:51 +0530

    Pish

--- 1 Arrays & Objects/script.js
+++ 1 Arrays & Objects/script.js
@@ -1,28 +1,36 @@
-//Push
+// //Push
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8];
+// const array = [1, 2, 3, 4, 5, 6, 7, 8];
 
-// array.push(9);
+// // array.push(9);
+// // console.log(array);
+// // array.pop(9);
+// // console.log(array);
+
+// //shift
+// let shiftEl = array.shift();
+// console.log(shiftEl);
 // console.log(array);
-// array.pop(9);
-// console.log(array);
 
-//shift
-let shiftEl = array.shift();
-console.log(shiftEl);
+// const numbersShift = [100, 200, 300, 400, 500];
+// let number;
+// while ((number = numbersShift.shift()) !== undefined) {
+//   console.log(number);
+// }
+// console.log(numbersShift);
+
+// //unshift
+// const numbersUShift = [100, 200, 300, 400, 500];
+// let numberU = 0;
+// numbersUShift.unshift(numberU);
+// console.log(numbersUShift);
+
+// //Slice Method
+//Splice
+
+const array = [2, 3, 4, 5, 6, 7, 8, 9];
+array.splice(8, 0, 11, 22);
 console.log(array);
-
-const numbersShift = [100, 200, 300, 400, 500];
-let number;
-while ((number = numbersShift.shift()) !== undefined) {
-  console.log(number);
-}
-console.log(numbersShift);
-
-//unshift
-const numbersUShift = [100, 200, 300, 400, 500];
-let numberU = 0;
-numbersUShift.unshift(numberU);
-console.log(numbersUShift);
-
-//Slice Method
+let languages = ['C', 'C++', 'Java', 'JavaScript'];
+languages.splice(0, 2, 'Python', 'Ruby', '');
+console.log(languages);
