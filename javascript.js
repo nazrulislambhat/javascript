2c27c08e354d536ee7fbea5a0979b35b73a18295
Nazrul Islam <nazrulislambhat@gmail.com>
2025-08-22 09:58:48 +0530

    Some Changes related to footer

--- javascript.js
+++ javascript.js
@@ -1,8 +1,14 @@
 const arr = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 55, 43, 1234, 5, 432, 23, 34, 5456, 54, 456, 4356,
-  12, 1, 42, 345, 456, 74,
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 55, 43, 12, 34, 5, 44, 32, 23, 34, 54, 56, 54, 45,
+  26, 43, 55, 12, 41, 42, 53, 45, 34, 56, 74,
 ];
 
+const users = [
+  { firstName: 'nazrul', lastName: 'islam', age: 34 },
+  { firstName: 'abdullah', lastName: 'islam', age: 2 },
+  { firstName: 'sumaiya', lastName: 'magray', age: 34 },
+  { firstName: 'suwaiba', lastName: 'islam', age: 0 },
+];
 // const double = arr.map(function (x) {
 //   return x * 2;
 // });
@@ -11,5 +17,21 @@ const arr = [
 // const binary = arr.map((x) => x.toString(2));
 // console.log(binary);
 // console.log(arr);
-const even = arr.filter((x) => x % 2 == 0);
-console.log(even);
+
+// const even = arr.filter((x) => x % 2 === 0);
+// console.log(even);
+// function findSum(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum = sum + arr[i];
+//   }
+//   return sum;
+// }
+// console.log(findSum(arr));
+
+// const outpur = arr.reduce(function (acc, current) {});
+
+const fullnames = users.map((x) => x.firstName + ' ' + x.lastName);
+console.log(fullnames);
+const aboveEighteen = users.map((x) => x.age > 18);
+console.log(aboveEighteen);
