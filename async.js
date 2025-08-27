3a47d2c8cf73e7ac57c67ee9e28bdf672cd72b88
Nazrul Islam <nazrulislambhat@gmail.com>
2025-08-27 06:49:23 +0530

    Some Changes related to footer

--- async.js
+++ async.js
@@ -1,3 +1,21 @@
 const p = new Promise((resolve, reject) => {
-  resolve('Promise is Resolved');
+  setTimeout(() => {
+    resolve('Promise is Resolved');
+  }, 5000);
 });
+
+async function handlePromise() {
+  const val = await p;
+  //our js engine was waiting for promise to resolved
+  console.log('before handle promise');
+  console.log(val);
+  console.log('after handle promise');
+}
+handlePromise();
+
+// function getData() {
+// JS Engine will not wait for the promise to be resolved
+//   p.then((res) => console.log(res));
+//   console.log('getdata funtion');
+// }
+// getData();
