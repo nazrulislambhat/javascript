74e5056317d7e3f4634338afc1c642d4837e779e
Nazrul Islam <nazrulislambhat@gmail.com>
2025-09-04 11:11:57 +0530

    Some Changes related to footer

--- fetch.js
+++ fetch.js
@@ -1,15 +1,15 @@
-const API_URL = 'https://api.github.com/users/atlassian';
-async function handlePromise() {
-  //returns a response object. it has a body that is a readable stream. To convert it into json tyou will have to do .json & that json is also a promise. once resolved it will give the value. whereever you have async code wrap it insde a try block
+// const API_URL = 'https://api.github.com/users/atlassian';
+// async function handlePromise() {
+//   //returns a response object. it has a body that is a readable stream. To convert it into json tyou will have to do .json & that json is also a promise. once resolved it will give the value. whereever you have async code wrap it insde a try block
 
-  //fetch().then(res => res.json()).then(res => console.log(res))
-  try {
-    const data = await fetch(API_URL);
-    const jsonValue = await data.json();
+//   //fetch().then(res => res.json()).then(res => console.log(res))
+//   try {
+//     const data = await fetch(API_URL);
+//     const jsonValue = await data.json();
 
-    console.log(jsonValue);
-  } catch (err) {
-    console.log(err);
-  }
-}
-handlePromise();
+//     console.log(jsonValue);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+// handlePromise();
