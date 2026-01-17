ff54d592a1f466873e0980e37e163ad6db6b1104
Nazrul Islam <nazrulislambhat@gmail.com>
2026-01-17 10:19:42 +0530

    a

--- tabs.js
+++ tabs.js
@@ -1,51 +1,7 @@
-// // let buttons = document.querySelectorAll('.tab-button');
-// // let panels = document.querySelectorAll('.tab-panel');
-
-// // buttons.forEach((button, index) => {
-// //   button.addEventListener('click', () => {
-// //     //remove active from all buttons
-// //     buttons.forEach((btn) => btn.classList.remove('active'));
-
-// //     //remove active from all panels
-// //     panels.forEach((panel) => panel.classList.remove('active'));
-
-// //     //add to clicked button
-// //     button.classList.add('active');
-// //     //activate matching panel
-
-// //     panels[index].classList.add('active');
-// //   });
-// // });
-// //WITHOUT RELYING ON DOM ORDER
-
-// let buttons = document.querySelectorAll('.tab-button');
-// let panels = document.querySelectorAll('.tab-panel');
-
-// buttons.forEach((button) => {
-//   button.addEventListener('click', () => {
-//     const target = button.dataset.tab;
-
-//     //remove active from all buttons
-//     buttons.forEach((btn) => btn.classList.remove('active'));
-
-//     //remove active from all panels
-//     panels.forEach((panel) => panel.classList.remove('active'));
-
-//     //add to clicked button
-//     button.classList.add('active');
-
-//     document
-//       .querySelector(`.tab-panel[data-panel="${target}"]`)
-//       .classList.add('active');
-//   });
-// });
-
 const tabs = document.querySelector('.tabs');
 
 tabs.addEventListener('click', (e) => {
   const button = e.target.closest('.tab-button');
-
-  //click wasnt on tab button
 
   if (!button || !tabs.contains(button)) return;
 
@@ -53,14 +9,13 @@ tabs.addEventListener('click', (e) => {
 
   const panel = tabs.querySelector(`.tab-panel[data-panel="${target}"]`);
 
-  if (!panel) {
-    console.error(`No Panel Found ${target}`);
-  }
+  if (!panel) return;
 
   tabs
     .querySelectorAll('.tab-button')
-    .forEach((button) => button.classList.remove('active'));
+    .forEach((btn) => btn.classList.remove('active'));
 
+  // reset panels
   tabs
     .querySelectorAll('.tab-panel')
     .forEach((p) => p.classList.remove('active'));
